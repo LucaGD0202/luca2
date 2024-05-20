4597cecf32c257258df93fc74c6b250137ea1cd6
LucaGD0202 <ctrlplay.boavista.ctba@gmail.com>
2024-05-20 19:55:53 -0300

    1.0

---
+++ g.py
@@ -0,0 +1,6 @@
+jogo=float(input("de 1 a 10 qual foi a sua nota na prova"))
+jogo2=float(input("de 1 a 10 qual foi a sua nota na segunda prova"))
+if jogo + jogo2 < 12:
+     print("reprovado")    
+else:
+    print("aprovado")
